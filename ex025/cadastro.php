cd7b9d121e467a6e00a0c86bbf9f810c7f1097f4
Rodrigo3103 <rodrigo.rrrryu@gmail.com>
2023-06-02 16:37:24 -0300

    update

---
+++ ex025/cadastro.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro PHP</title>
+</head>
+<body>
+    <p>cadastro php para aula de html</p>
+</body>
+</html>
